99a42e8e056506dd91b0f246464da102ecf922cd
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-01-02 09:45:20 +0600

    Retrieve all data and single data get api set

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.malve12.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 const dbConnect = async () => {
@@ -36,42 +37,55 @@ app.post('/api/v1/notes', async (req, res) => {
         content: req.body.content,
         created_at: new Date(),
         updated_at: new Date()
-      };
+    };
 
-      try {
+    try {
         const result = await takingDatabase.insertOne(note);
         res.send(result);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-  });
-  
-  // Retrieve Notes
-  app.get('/api/v1/notes', async (req, res) => {
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+// Retrieve Notes
+app.get('/api/v1/notes', async (req, res) => {
+    try {
+        const result = await takingDatabase.find().toArray();
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal server error' });
+    }
+});
+
+// Retrieve Single Note by ID
+app.get('/api/v1/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    try {
+        const result = await takingDatabase.findOne(query)
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal server error' });
+    }
+});
+
+// Update Note
+app.put('/api/v1/notes/:id', async (req, res) => {
+    const id = req.params.id;
+
+});
+
+// Delete Note
+app.delete('/api/v1/notes/:id', async (req, res) => {
     // ...
-  });
-  
-  // Retrieve Single Note by ID
-  app.get('/api/v1/notes/:id', async (req, res) => {
-    // ...
-  });
-  
-  // Update Note
-  app.put('/api/v1/notes/:id', async (req, res) => {
-    // ...
-  });
-  
-  // Delete Note
-  app.delete('/api/v1/notes/:id', async (req, res) => {
-    // ...
-  });
-  
+});
 
 
-app.get('/', (req,res)=>{
+
+app.get('/', (req, res) => {
     res.send('Welcome to my server!')
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`star my server on port ${port}`);
 })
